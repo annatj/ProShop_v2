234af5e1381613656e09fb6e608e9a356d4cd942
Anna <annajoy20001@gmail.com>
2023-11-03 00:41:14 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
-    <p>Welcome to proshop</p>
+    <>
+    <Header/>
+    <main className='py-3'>
+      <Container>
+    <Outlet/>
+      </Container>
+
+    </main>
+    <Footer/>
+    <ToastContainer/>
+    </>
   )
 }
 
